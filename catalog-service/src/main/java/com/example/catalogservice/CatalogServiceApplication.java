3b09698c1af63e6e9934239b3d31e8b4b3d208e4
srss-pocs <145287517+srss-pocs@users.noreply.github.com>
2023-10-22 21:55:49 +0530

    user service modified

--- catalog-service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.resiliency;
+package com.example.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ResiliencyApplication {
+public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ResiliencyApplication.class, args);
+		SpringApplication.run(CatalogServiceApplication.class, args);
 	}
 
 }
--- catalog-service/src/test/java/com/example/catalogservice/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/example/catalogservice/CatalogServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.resiliency;
+package com.example.catalogservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ResiliencyApplicationTests {
+class CatalogServiceApplicationTests {
 
 	@Test
 	void contextLoads() {

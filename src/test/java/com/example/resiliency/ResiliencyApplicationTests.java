1b42e0cfab6f338f9027ad68e6bfca22248ba459
srss-pocs <145287517+srss-pocs@users.noreply.github.com>
2023-10-22 21:17:03 +0530

    Initial commit

---
+++ src/test/java/com/example/resiliency/ResiliencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resiliency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResiliencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
